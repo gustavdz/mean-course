2fe5248f371ffadda90038b3102fd266c5865f37
Gustavo Decker Zambrano <gustavdz@gmail.com>
2019-10-13 16:49:56 -0500

    Lesson 41
    Section 3 Completed

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,29 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extend: false }));
+
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin','*');
-  res.setHeader('Access-Control-Allow-Header','Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept');
   res.setHeader('Access-Control-Allow-Methods','GET, POST, PATCH, DELETE, OPTIONS');
   next();
 });
 
-app.use('/api/posts',(req, res, next) => {
+
+app.post('/api/posts',(req, res, next) => {
+  const post = req.body;
+  console.log(post);
+  res.status(201).json({
+    message: 'Post added successfully.',
+  });
+});
+
+
+app.get('/api/posts',(req, res, next) => {
   const posts = [
     {
       id: 'fad123123',
@@ -24,11 +38,11 @@ app.use('/api/posts',(req, res, next) => {
     {
       id: 'fad543',
       title: 'Third server-side post',
-      content: 'This is comming from the server actually too'
+      content: 'This is coming from the server actually too'
     },
   ];
   res.status(200).json({
-    message: 'Posts fetch succesfully!',
+    message: 'Posts fetch successfully!',
     posts: posts
   });
 });
