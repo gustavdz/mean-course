f87f4c7186f049d1f9502a4964212eb9d7f9c7e9
Gustavo Decker Zambrano <gustavdz@gmail.com>
2019-10-13 02:19:23 -0500

    lesson 27

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,7 +1,7 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import {Component} from '@angular/core';
 import {NgForm} from '@angular/forms';
 
-import { Post } from '../post.model';
+import {PostsService} from '../posts.service';
 
 @Component({
   selector: 'app-post-create',
@@ -9,19 +9,17 @@ import { Post } from '../post.model';
   styleUrls: ['./post-create.component.css']
 })
 export class PostCreateComponent {
-  enteredContent = '';
-  enteredTitle = '';
-  @Output() postCreated = new EventEmitter<Post>();
+
+
+  constructor(public postsService: PostsService) {}
 
   onAddPost(form: NgForm) {
-    if (form.invalid){
+    if (form.invalid) {
       return;
     }
-    const post: Post = {
-      title: form.value.title,
-      content: form.value.content
-    };
-    this.postCreated.emit(post);
+    this.postsService.addPost(form.value.title, form.value.content);
   }
+
+
 
 }
