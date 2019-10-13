5f077dd4768f1211e465f3d25545bfe3470bc64f
Gustavo Decker Zambrano <gustavdz@gmail.com>
2019-10-13 13:27:27 -0500

    lesson 37

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,28 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('First middleware');
-  next();
-});
-
-app.use((req, res, next) => {
-  res.send('Hello from express!');
+app.use('/api/posts',(req, res, next) => {
+  const posts = [
+    {
+      id: 'fad123123',
+      title: 'First server-side post',
+      content: 'This is comming from the server'
+    },
+    {
+      id: 'fad321321',
+      title: 'Second server-side post',
+      content: 'This is comming from the server too'
+    },
+    {
+      id: 'fad543',
+      title: 'Third server-side post',
+      content: 'This is comming from the server actually too'
+    },
+  ];
+  res.status(200).json({
+    message: 'Posts fetch succesfully!',
+    posts: posts
+  });
 });
 
 module.exports = app;
